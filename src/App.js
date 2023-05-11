1722245b0bc07f98054959473ec95b90a267d5da
89YAMAL <alex-popov1987@yandex.ru>
2023-05-11 18:31:23 +0500

    finish4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import VideoList from './components/VideoList';
+import moment from 'moment/moment';
 
 export default function App() {
     const list = [
         {
             url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2023-05-10 18:24:00'
+            date: moment(new Date())
+            .subtract(10, "minutes")
+            .format("YYYY-MM-DD HH:mm:ss")
+            .toString(),
         },
         {
             url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2023-05-10 12:10:00'
+            date: moment(new Date())
+            .subtract(65, "minutes")
+            .format("YYYY-MM-DD HH:mm:ss")
+            .toString(),
         },
         {
             url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
@@ -17,7 +24,10 @@ export default function App() {
         },
         {
             url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
-            date: '2018-01-03 12:10:00'
+            date: moment(new Date())
+            .subtract(28, "hours")
+            .format("YYYY-MM-DD HH:mm:ss")
+            .toString(),
         },
         {
             url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
